d72a0713bea8ed004edc9aae1de2af81ad328306
Greg Retkowski <greg@rage.net>
2023-10-08 19:50:57 -0700

    well at this point it assembles still todos!

--- monitor.asm
+++ monitor.asm
@@ -14,14 +14,14 @@ mainprog:
 
 ; Page 0 Variables
 
-XAML            = 0x24           ;  Last "opened" location Low
-XAMH            = 0x25           ;  Last "opened" location High
-STL             = 0x26           ;  Store address Low
-STH             = 0x27           ;  Store address High
-L               = 0x28           ;  Hex value parsing Low
-H               = 0x29           ;  Hex value parsing High
-YSAV            = 0x2A           ;  Used to see if hex value is given
-MODE            = 0x2B           ;  $00=XAM, $7F=STOR, $AE=BLOCK XAM
+XAML            = 0x0024           ;  Last "opened" location Low
+XAMH            = 0x0025           ;  Last "opened" location High
+STL             = 0x0026           ;  Store address Low
+STH             = 0x0027           ;  Store address High
+L               = 0x0028           ;  Hex value parsing Low
+H               = 0x0029           ;  Hex value parsing High
+YSAV            = 0x002A           ;  Used to see if hex value is given
+MODE            = 0x002B           ;  $00=XAM, $7F=STOR, $AE=BLOCK XAM
 
 terminal        = 0x8001
 keyready        = 0x8003
@@ -30,8 +30,17 @@ keyread         = 0x8002
 
 IN              = 0x0200         ;  Input buffer to $027F
 
-inbuf             = 0x0030         ; pointer to the input buffer
+inbuf             = 0x0030       ; pointer to the input buffer
 inbufh            = 0x0031
+
+XTR               = 0x0041       ; Place to store X register when handling text buffer
+XMR               = 0x0042       ; Place to store X register when handling memory locations
+
+; These values, minus one! used in SETADR
+L_M    = 0x0027
+STL_M  = 0x0025
+XAML_M = 0x0023
+
 
 
 RESET:
@@ -39,18 +48,19 @@ RESET:
                 STA inbuf
                 LDI >IN
                 STA inbufh
+
                 
 
 ; Handle text input
-NOTCR:          CMP 8          ; Backspace?
+NOTCR:          CMP 8           ; Backspace?
                 BEQ BACKSPACE   ; Yes.
                 CMP 27          ; ESC?
                 BEQ ESCAPE      ; Yes.
                 INX             ; Advance text index.
-                ; BPL NEXTCHAR    ; Auto ESC if > 127.
-ESCAPE:         LDI '\'    ; "\".
+                ; BPL NEXTCHAR  ; Auto ESC if > 127.
+ESCAPE:         LDI '\'         ; "\".
                 JSR ECHO        ; Output it.
-GETLINE:        LDI '\n'        ; CR.
+GETLINE:        LDI 10          ; CR.
                 JSR ECHO        ; Output it.
                 LDI 0x01        ; Initialize text index.
                 TAX
@@ -69,90 +79,137 @@ NEXTCHAR:       LDA keyready    ; Key ready?
 ; Drop though, got a CR! now we are processing the buffer
 DONEINPUT:
 
+; Y reg is used as an index into the textstring we got
+; X reg is used to interact with the mem location we are working with
+; Acc is used for math scratch etc.
+
                 LDI 0xFF        ; Reset text index.
                 TAX
                 LDA 0x00        ; For XAM mode.
                 TAX             ; 0->X.
+                ; todo are we mem reg or text reg here ^^^
 SETSTOR:        ASL             ; Leaves $7B if setting STOR mode.
 SETMODE:        STA MODE        ; $00=XAM $7B=STOR $AE=BLOK XAM
-BLSKIP:         INY             ; Advance text index.
-NEXTITEM:       LDA IN,Y        ; Get character.
-                CMP #$8D        ; CR?
+BLSKIP:         LDX XTR         ; Advance text index.
+                INX
+NEXTITEM:       LPX inbuf       ; Get character.
+                CMP 10          ; CR?
                 BEQ GETLINE     ; Yes, done this line.
-                CMP #'.'+$80    ; "."?
-                BCC BLSKIP      ; Skip delimiter.
+                CMP '.'         ; "."?
+                BCS X_BCC_BSK   ; Skip delimiter.
+                JMP BLSKIP
+X_BCC_BSK:
                 BEQ SETMODE     ; Yes. Set STOR mode.
-                CMP #':'+$80    ; ":"?
+                CMP ':'         ; ":"?
                 BEQ SETSTOR     ; Yes. Set STOR mode.
-                CMP #'R'+$80    ; "R"?
+                CMP 'R'         ; "R"?
                 BEQ RUN         ; Yes. Run user program.
+                STX XTR
+                LDX XMR
                 STX L           ; $00-> L.
                 STX H           ; and H.
-                STY YSAV        ; Save Y for comparison.
-NEXTHEX:        LDA IN,Y        ; Get character for hex test.
-                EOR #$B0        ; Map digits to $0-9.
-                CMP #$0A        ; Digit?
-                BCC DIG         ; Yes.
-                ADC #$88        ; Map letter "A"-"F" to $FA-FF.
-                CMP #$FA        ; Hex letter?
-                BCC NOTHEX      ; No, character not hex.
+                STX XMR
+                LDX XTR
+                STX YSAV        ; Save Y for comparison.
+NEXTHEX:        LPX inbuf       ; Get character for hex test.
+                XOR 0xB0        ; Map digits to $0-9.
+                CMP 0x0A        ; Digit?
+                BCS X_BCC_DIG
+                JMP DIG         ; Yes.
+X_BCC_DIG:
+                ADD 0x88        ; Map letter "A"-"F" to $FA-FF.
+                CMP 0xFA        ; Hex letter?
+                BCS X_BCC_NOTHEX
+                JMP NOTHEX      ; No, character not hex.
+X_BCC_NOTHEX:
 DIG:            ASL
                 ASL             ; Hex digit to MSD of A.
                 ASL
                 ASL
-                LDX #$04        ; Shift count.
+                STX XTR
+                LDX 0x04        ; Shift count. (?)
+                
 HEXSHIFT:       ASL             ; Hex digit left, MSB to carry.
-                ROL L           ; Rotate into LSD.
-                ROL H           ;  Rotate into MSD’s.
+                LDA L
+                ASL             ; Rotate into LSD.
+                STA L
+                LDA H
+                ASL             ;  Rotate into MSD’s.
+                STA H
                 DEX             ; Done 4 shifts?
-                BNE HEXSHIFT    ; No, loop.
-                INY             ; Advance text index.
-                BNE NEXTHEX     ; Always taken. Check next char for hex.
-NOTHEX:         CPY YSAV        ; Check if L, H empty (no hex digits).
+                BEQ X_BNE_HS
+                JMP HEXSHIFT    ; No, loop.
+X_BNE_HS:
+                STX XMR
+                LDX XTR
+                INX             ; Advance text index.
+                BEQ X_BNE_NH
+                JMP NEXTHEX     ; Always taken. Check next char for hex.
+X_BNE_NH:
+NOTHEX:         LDI 0x00        ; Check if L, H empty (no hex digits).
+                CMP YSAV
                 BEQ ESCAPE      ; Yes, generate ESC sequence.
-                BIT MODE        ; Test MODE byte.
-                BVC NOTSTOR     ;  B6=0 STOR 1 for XAM & BLOCK XAM
+;          -      BIT MODE        ; Test MODE byte.
+;          -      BVC NOTSTOR     ;  B6=0 STOR 1 for XAM & BLOCK XAM
                 LDA L           ; LSD’s of hex data.
-                STA (STL,X)     ; Store at current ‘store index’.
-                INC STL         ; Increment store index.
-                BNE NEXTITEM    ; Get next item. (no carry).
-                INC STH         ; Add carry to ‘store index’ high order.
+                LDX XMR
+                SPX STL         ; Store at current ‘store index’.
+                LDX STL         ; Increment store index.
+                INX
+                STX STL
+                BEQ X_BNE_NEXTI
+                JMP NEXTITEM    ; Get next item. (no carry).
+X_BNE_NEXTI:
+                LDX STH         ; Add carry to ‘store index’ high order.
+                INX
+                STX STH
 TONEXTITEM:     JMP NEXTITEM    ; Get next command item.
 RUN:            LDI 0x00        ; Run at current XAM index.
                 TAX
                 JPX XAML
-NOTSTOR:        BMI XAMNEXT     ; B7=0 for XAM, 1 for BLOCK XAM.
-                LDX #$02        ; Byte count.
-SETADR:         LDA L-1,X       ; Copy hex data to
-                STA STL-1,X     ; ‘store index’.
-                STA XAML-1,X    ; And to ‘XAM index’.
+NOTSTOR:    ; -  BMI XAMNEXT     ; B7=0 for XAM, 1 for BLOCK XAM.
+                LDX 0x02        ; Byte count.
+                
+SETADR:         LDX XMR
+                LPX L_M       ; Copy hex data to
+                SPX STL_M     ; ‘store index’.
+                SPX XAML_M    ; And to ‘XAM index’.
                 DEX             ; Next of 2 bytes.
-                BNE SETADR      ; Loop unless X=0.
-NXTPRNT:        BNE PRDATA      ; NE means no address to print.
-                LDA #$8D        ; CR.
+                BEQ NXTPRNT
+                JMP SETADR      ; Loop unless X=0.
+NXTPRNT:        BEQ X_BNE_PRDATA
+                JMP PRDATA      ; NE means no address to print.
+X_BNE_PRDATA:
+                LDA 10          ; CR.
                 JSR ECHO        ; Output it.
                 LDA XAMH        ; ‘Examine index’ high-order byte.
                 JSR PRBYTE      ; Output it in hex format.
                 LDA XAML        ; Low-order ‘examine index’ byte.
                 JSR PRBYTE      ; Output it in hex format.
-                LDA #':'+$80    ; ":".
+                LDA ':'         ; ":".
                 JSR ECHO        ; Output it.
-PRDATA:         LDA #$A0        ; Blank.
+PRDATA:         LDA ' '         ; Blank.
                 JSR ECHO        ; Output it.
-                LDA (XAML,X)    ; Get data byte at ‘examine index’.
+                LPX XAML        ; Get data byte at ‘examine index’.
                 JSR PRBYTE      ; Output it in hex format.
 XAMNEXT:        STX MODE        ; 0->MODE (XAM mode).
                 LDA XAML
                 CMP L           ; Compare ‘examine index’ to hex data.
                 LDA XAMH
-                SBC H
+;            --  SBC H  ; greg - this is doing some subtraction  to then cpompare, how can I rewrite it different?
                 BCS TONEXTITEM  ; Not less, so no more data to output.
-                INC XAML
-                BNE MOD8CHK     ; Increment ‘examine index’.
-                INC XAMH
+                LDA XAML
+                ADD 0x01
+                STA XAML
+                BEQ NO_MOD8CHK   ; Increment ‘examine index’.
+                JMP MOD8CHK
+NO_MOD8CHK:
+                LDA XAMH
+                ADD 0x01
+                STA XAMH
 MOD8CHK:        LDA XAML        ; Check low-order ‘examine index’ byte
-                AND #$07        ; For MOD 8=0
-                BPL NXTPRNT     ; Always taken.
+                AND 0x07        ; For MOD 8=0
+                JMP NXTPRNT     ; Always taken.
 
 ; Functions to output to screen, incl conversion from value to ascii chars.
 PRBYTE:         PHA             ; Save A for LSD.

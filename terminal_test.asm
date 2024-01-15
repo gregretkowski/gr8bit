d566a9a65ec5dae114766c8747c9f45df55776f3
Greg Retkowski <greg@rage.net>
2024-01-14 21:03:31 -0800

    small change

--- terminal_test.asm
+++ terminal_test.asm
@@ -8,6 +8,7 @@ keybuffer=0x1000
 
 mainprog:
 
+    ; print a string to the terminal.
     LDI <string
     STA 0x0080
     LDI >string
@@ -15,12 +16,14 @@ mainprog:
     JSR std_lineout
     LDI '>'
     STA terminal
+
     ; use a keybuffer at 0x1000
     LDI 0x00
     STA 0x0080
     LDI 0x10
     STA 0x0081
     JSR std_readline
+
     ; should be able to use the same addr and print it again!
     JSR std_lineout
     LDI '!'
